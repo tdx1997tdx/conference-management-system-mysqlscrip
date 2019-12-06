8ac8582d9816d5ab193618be2eada889f0eb3358
TangDexuan <798637048@qq.com>
2019-12-06 12:41:03 +0800

    添加user_and_meeting表

---
+++ src/main/resources/dbscript/04_createUserAndMeetingTable.sql
@@ -0,0 +1,13 @@
+-- auto-generated definition
+create table user_and_meeting
+(
+  user_id    int not null,
+  meeting_id int not null,
+  primary key (user_id, meeting_id),
+  constraint user_and_meeting_meeting_meeting_id_fk
+    foreign key (meeting_id) references meeting (meeting_id)
+      on update cascade on delete cascade,
+  constraint user_and_meeting_user_user_id_fk
+    foreign key (user_id) references user (user_id)
+      on update cascade on delete cascade
+);
