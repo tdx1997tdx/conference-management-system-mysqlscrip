e583768dd4641e9d40ba5d9463241b8bdbf1f217
handsome711 <handsome.abc711@gmail.com>
2019-12-13 19:57:33 +0800

    modify the table of device and delete the link table of room and device

--- src/main/resources/dbscript/04_createDeviceTable.sql
+++ src/main/resources/dbscript/04_createDeviceTable.sql
@@ -5,8 +5,11 @@ create table device
     primary key,
   form_name   varchar(20)                        null,
   reason      varchar(20)                        null,
-  room_name   varchar(20)                        null,
+  room_id     int                                null,
   create_time datetime default CURRENT_TIMESTAMP not null,
   repair_time datetime default CURRENT_TIMESTAMP not null,
-  state int
+  state        int                                null,
+  constraint room_fk
+  foreign key (room_id) references room (room_id)
+    on update cascade on delete cascade
 );
