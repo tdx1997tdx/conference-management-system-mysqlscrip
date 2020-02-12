479dbb748f48965e7dca1f5cfed3271a952a43ab
handsome711 <handsome.abc711@gmail.com>
2020-02-12 22:12:25 +0800

    add a new table "device" and the link table of room and device

--- src/main/resources/dbscript/04_createDeviceTable.sql
+++ src/main/resources/dbscript/04_createDeviceTable.sql
@@ -3,12 +3,15 @@ create table device
 (
   device_id   int auto_increment
     primary key,
-  form_name   varchar(20)                        null,
-  reason      varchar(20)                        null,
-  room_id     int                                null,
-  create_time datetime default CURRENT_TIMESTAMP not null,
-  repair_time datetime default CURRENT_TIMESTAMP not null,
-  state        int                                null,
+
+  device_name   varchar(20)                     not null,
+  brand         varchar(20)                     null,
+  type          varchar(20)                     null,
+  repair_times  int                             null,
+  room_id       int                             not null,
+  mttr          time                            null ,
+  mtbf          time                            null ,
+
   constraint room_fk
   foreign key (room_id) references room (room_id)
     on update cascade on delete cascade
