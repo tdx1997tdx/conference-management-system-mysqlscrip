96a1b27ab9a98a47a3459bca645468b5e0c6c84f
handsome711 <handsome.abc711@gmail.com>
2020-02-26 17:17:59 +0800

    修改了message表的列名，添加了初始数据

--- src/main/resources/dbscript/08_createMessageTable.sql
+++ src/main/resources/dbscript/08_createMessageTable.sql
@@ -5,7 +5,7 @@ create table message
     primary key (message_id),
   message_topic varchar(30)                        null,
   message_body  varchar(100)                       null,
-  sendTime      datetime default CURRENT_TIMESTAMP null,
+  send_time     datetime default CURRENT_TIMESTAMP null,
   sender        varchar(20)                        null,
   receiver      varchar(20)                        null
 );
