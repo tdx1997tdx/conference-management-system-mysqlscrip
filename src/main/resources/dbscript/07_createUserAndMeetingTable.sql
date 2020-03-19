30e1b0c76e236ea084cd51bd065b0f2aaaef750b
TangDexuan <798637048@qq.com>
2020-03-19 16:26:10 +0800

    修改user_and_meeting表

--- src/main/resources/dbscript/07_createUserAndMeetingTable.sql
+++ src/main/resources/dbscript/07_createUserAndMeetingTable.sql
@@ -1,10 +1,9 @@
 -- auto-generated definition
 create table user_and_meeting
 (
-  user_id    int          not null,
-  meeting_id int          not null,
-  state      int          null,
-  message    varchar(100) null,
+  user_id    int           not null,
+  meeting_id int           not null,
+  state      int default 2 null,
   primary key (user_id, meeting_id),
   constraint user_and_meeting_meeting_meeting_id_fk
     foreign key (meeting_id) references meeting (meeting_id)
