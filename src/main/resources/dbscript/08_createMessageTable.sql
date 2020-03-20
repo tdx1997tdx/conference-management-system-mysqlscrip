4d404f3dcc0de9de1c7a017d4bc2e52a131513cb
TangDexuan <798637048@qq.com>
2020-03-20 19:41:45 +0800

    修改message表,message_body的长度

--- src/main/resources/dbscript/08_createMessageTable.sql
+++ src/main/resources/dbscript/08_createMessageTable.sql
@@ -1,12 +1,11 @@
 -- auto-generated definition
 create table message
 (
-  message_id    int auto_increment,
-    primary key (message_id),
-  message_topic varchar(30)                        null,
-  message_body  varchar(100)                       null,
-  send_time     datetime default CURRENT_TIMESTAMP null,
-  sender_name   varchar(20) default "System"       null,
-  receiver_name varchar(20)                        null
+  message_id    int auto_increment
+    primary key,
+  message_topic varchar(30)                           null,
+  message_body  varchar(200)                          null,
+  send_time     datetime    default CURRENT_TIMESTAMP null,
+  sender_name   varchar(20) default 'System'          null,
+  receiver_name varchar(20)                           null
 );
-
